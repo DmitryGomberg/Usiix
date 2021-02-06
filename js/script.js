fbbf01b9b390172c1a41541263e7b631d7eef33f
Dmitry <dpgomberg@gmail.com>
2021-02-06 19:46:16 +0300

    Верстка первой секции с адаптивом

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
    $('.menu__open-close, .menu__burger').click(function(event){
        $('.menu__open').toggleClass('active');
-       $('body').toggleClass('lock');
    });
 });
