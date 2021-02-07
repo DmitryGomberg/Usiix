4d093f5a6a8749ba77dcc29b277b766bc3832664
Dmitry <dpgomberg@gmail.com>
2021-02-07 22:48:30 +0300

    Адаптивная верстка первого слайдера

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@ $(document).ready(function(){
        $('.menu__open').toggleClass('active');
    });
 });
+
+const swiper = new Swiper('.s6__swiper-container', {
+   spaceBetween: 30,
+
+   pagination: {
+     el: '.swiper-pagination',
+     clickable: true
+   },
+   
+   // Navigation arrows
+   navigation: {
+     nextEl: '.swiper-button-next',
+     prevEl: '.swiper-button-prev',
+   },
+ });
